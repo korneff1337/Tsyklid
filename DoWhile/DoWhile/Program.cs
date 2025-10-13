7561f9e8b57386cdfacf93141a0fa767e9e52522
korneff1337 <74781679+korneff1337@users.noreply.github.com>
2025-10-13 09:12:17 +0300

    do while joonistab ruutu

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string kasutajaNimi = "";
+do
+{
+    Console.WriteLine("Palun sisesta oma kasutajanimi");
+    kasutajaNimi = Console.ReadLine();
+}
+while (kasutajaNimi != "user1");
+if (kasutajaNimi == "user1")
+{
+    int ruuduSuurus = 0;
+
+    Console.WriteLine("Kui suurt ruutu tahad?");
+
+    do
+    {
+        Console.WriteLine("Kui suurt ruutu tahad?");
+        ruuduSuurus = int.Parse(Console.ReadLine());
+    } while (ruuduSuurus < 0 && ruuduSuurus > 20);
+    char reaKujund = '█';
+    string üksRida = "";
+    int tsükliMuutuja = ruuduSuurus;
+    do
+    {
+        üksRida += reaKujund;
+        tsükliMuutuja -= 1;
+    } while (tsükliMuutuja != 0);
+    tsükliMuutuja = ruuduSuurus;
+    do
+    {
+        Console.WriteLine(üksRida);
+        tsükliMuutuja -= 1;
+
+    } while (tsükliMuutuja != 0);
+}
