30b31c0274ea2c776b6b65cd5a0ec8ca3558571d
Opilane TTHK <74781679+korneff1337@users.noreply.github.com>
2025-10-27 11:47:44 +0200

    Kodutöö

--- For/For/Program.cs
+++ For/For/Program.cs
@@ -13,15 +13,51 @@
 //string temp = "no name"; //muutuja mis hetkel on "tühi"
 //while (objectCount < 5) //while tsükkel mis küsib nimesid
 //{
-//    Console.WriteLine("Sisesta järgmine lemmikloomanimi");
-//    temp = Console.ReadLine(); // temp muutujasse väärtuse (mida me tahame) sisestamine
-//    objectArray[objectCount] = temp; // massiivi, asukohal objectCount asetame ajutisest muutujast väärtuse
-//    objectCount++; // tsükli inkrementatsioon
+//   Console.WriteLine("Sisesta järgmine lemmikloomanimi");
+//   temp = Console.ReadLine(); // temp muutujasse väärtuse (mida me tahame) sisestamine
+//   objectArray[objectCount] = temp; // massiivi, asukohal objectCount asetame ajutisest muutujast väärtuse
+//   objectCount++; // tsükli inkrementatsioon
 //}
 //Console.WriteLine("Oled sisestanud järgnevad objektid"); // sõnum
 //for (int i = 0; i < objectArray.Length; i++)
 //{
 //    Console.WriteLine($"{i + 1}. element on: {objectArray[i]}");
-//    // kuvame välja reanumbri koos vastava elemendiga, muutuja i abil.
+//   // kuvame välja reanumbri koos vastava elemendiga, muutuja i abil.
 //}
 //Console.ReadLine(); // peatame programmi klahvisisestuse taha
+
+string kasutajaNimi = "";
+Console.WriteLine("Palun sisesta oma kasutajanimi");
+do
+{
+    kasutajaNimi = Console.ReadLine();
+    Console.WriteLine("See polnuid õige, palun sisesta oma kasutajanimi");
+}
+while (kasutajaNimi != "Korneff");
+{
+    Console.WriteLine("Kasutaja on sisse logitud");
+}
+if (kasutajaNimi == "Korneff")
+{
+    Console.WriteLine("Edukas sisselogimine.");
+}
+else
+{
+    Console.WriteLine("Kasutaja pole registreeritud.");
+}
+
+Console.WriteLine($"{kasutajaNimi}, et edasi minna peab sisestama parooli");
+string kasutajaParool = Console.ReadLine();
+if (kasutajaParool == "123")
+{
+    Console.WriteLine("Congratulations, parool on õige :D");
+}
+else if (kasutajaParool == "1234" || kasutajaParool == "12345")
+{
+    Console.WriteLine($"{kasutajaNimi}, parool on sobimatu :(");
+}
+else
+{
+    Console.WriteLine("Parool on vale");
+}
+
